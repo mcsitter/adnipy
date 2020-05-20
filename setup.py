d4554b9576c9c722a6d5678d01c98c8812b46a53
mcsitter <48606431+mcsitter@users.noreply.github.com>
2020-05-20 19:10:13 +0200

    Added testing for python 3.8
    Python 3.8 should be covered by the automated tests and the documentation.

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
     ],
     description="Process ADNI study data with adnipy.",
     python_requires=">=3.5.0",
