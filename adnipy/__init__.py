89465c2a67847fd658f6838e9b5141709d4abaa5
Maximilian Cosmo Sitter <48606431+mcsitter@users.noreply.github.com>
2026-05-07 19:21:55 +0200

    Bump version: 1.0.0 → 1.0.1

--- adnipy/__init__.py
+++ adnipy/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Maximilian Cosmo Sitter"""
 __email__ = "msitter@smail.uni-koeln.de"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["ADNI", "get_matching_images", "read_csv", "timedelta"]
 
 # Let users know if they're missing any of our hard dependencies
